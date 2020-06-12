eca76cf2c449f5895c4186bbb220d3586ec565ad
Jannis R <mail@jannisr.de>
2020-06-12 23:51:51 +0200

    add meta info to errors, 0.1.1

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ module.exports = (hostname, port, key) =>
 			hostname, port,
 			path: '/' + record
 		}, (res) => {
-
 			let body = ''
 			res.on('data', (chunk) => {body += chunk.toString()})
 			res.on('end', () => {
 				if (body === ip) yay(ip)
-				else nay(new Error('invalid body'))
+				else {
+					const msg = res.statusMessage || ' invalid response'
+					const err = new Error(msg)
+					err.statusCode = res.statusCode
+					err.headers = res.headers
+					nay(err)
+				}
 			})
 
 			res.on('errro', nay)
